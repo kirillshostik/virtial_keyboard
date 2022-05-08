3de8943fefd6cdda55f47112789b7fe54a1608b5
kirillshostik <kirillshotik@gmail.com>
2022-05-08 15:22:27 +0300

    feat: configure webpach and eslint

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, "src", "index.html")
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }
+        ]
+    }
+
+}
